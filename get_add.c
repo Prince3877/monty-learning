be4656fdbffdb2432bfb11bed51cffd8014586b2
Prince3877 <bernardwambua43@gmail.com>
2022-11-03 14:13:57 +0000

    get add

---
+++ get_add.c
@@ -0,0 +1,7 @@
+#include "monty.h"
+
+
+/**
+ * get_add - function that adds the top two elements of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: where the line number appears
